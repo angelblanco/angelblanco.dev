7227cf1c837301823bcad3f11bf8602a0f1b9083
ablanco <17175344+angelblanco@users.noreply.github.com>
2025-04-29 00:24:37 +0200

    👷 Home tech list

--- apps/angelblanco.dev/components/home/HomeTech.vue
+++ apps/angelblanco.dev/components/home/HomeTech.vue
@@ -1,7 +1,34 @@
 <template>
+  <div class="text-center  tect-primary-content">
+    <p class="max-w-ui overflow-hidden mx-auto text-4xl font-bold tracking-wide p-8 flex flex-col lg:flex-row flex-wrap items-center gap-4 justify-center">
+      Engenieer Manager <Icon name="heroicons:arrow-path" class="size-12 animate-spin-slow text-base-content" /> Full Stack Developer
+    </p>
+  </div>
+
+  <div class="max-w-2xl mx-auto text-pretty  leading-relaxed">
+    <p class="mb-8">
+      I'm a passionate <span class="font-extrabold gradient-text-alt2">Web</span> entushiast. As a profesional I enjoy <span class="font-bold gradient-text-alt">Team Management</span> as well as coding interactive web applications. Working on the hospitaility
+      industry for over 8 years, I've designed <span class="font-extrabold gradient-text">Big Data</span> multi-tenant architectures and face all the challenges of going from a Startup to a Scale up.
+    </p>
+
+    <p>
+      Through the years I've worked professionally with a lot of <span class="gradient-text-alt2 font-extrabold">awesome people</span> and technologies.
+    </p>
+  </div>
+
   <div class="max-w-ui mx-auto p-10 lg:p-20 mb-10 text-base-content/85">
+    <div class="flex flex-wrap justify-center items-center text-xs gap-4 mb-4">
+      <button
+        v-for="type in types" :key="type" class="font-mono cursor-pointer font-medium tracking-wide py-1 px-3 rounded-full transition"
+        :class="selectedType === type ? 'bg-primary text-primary-content' : 'bg-neutral text-neutral-content'"
+        @click="selectType(type)"
+      >
+        #{{ type }}
+      </button>
+    </div>
+
     <div class="flex flex-wrap gap-4 justify-center items-center">
-      <div v-for="(entry, index) in entries" :key="entry.title" class="border border-base-300 p-4 hover:border-primary hover:drop-shadow hover:drop-shadow-primary bg-base-100/90 group cursor-pointer" :class="selectedType && selectedType !== entry.type ? 'opacity-45' : ''" @click="selectType(entry)">
+      <div v-for="(entry, index) in entries" :key="entry.title" class="border border-base-300 p-4 transition hover:border-primary hover:drop-shadow hover:drop-shadow-primary bg-base-100/90 group cursor-pointer" :class="selectedType && selectedType !== entry.type ? 'opacity-45' : ''" @click="selectType(entry.type)">
         <div class="flex gap-4 items-center">
           <Icon class="transition size-8 flex-none" :class="(index % 2) === 0 ? 'group-hover:rotate-15' : 'group-hover:-rotate-15'" :name="entry.icon" :title="entry.title" />
           <div class="">
@@ -19,16 +46,16 @@
 </template>
 
 <script setup lang="ts">
-const { technologies: entries } = useTechList();
+const { technologies: entries, types } = useTechList();
 
 const selectedType = ref<TechType | null>(null);
 
-function selectType(entry: TechEntry) {
-  if (selectedType.value === entry.type) {
+function selectType(type: TechType) {
+  if (selectedType.value === type) {
     selectedType.value = null;
   }
   else {
-    selectedType.value = entry.type;
+    selectedType.value = type;
   }
 }
 </script>
--- apps/angelblanco.dev/composables/useTechList.ts
+++ apps/angelblanco.dev/composables/useTechList.ts
@@ -15,121 +15,132 @@ export interface TechEntry {
 
 export interface TechList {
   technologies: TechEntry[];
+  types: TechType[];
 }
 
 export default function useTechList(): TechList {
+  const technologies: TechEntry[] = [
+    {
+      title: 'Vue',
+      icon: 'simple-icons:vuedotjs',
+      type: TechType.Framework,
+    },
+    {
+      title: 'Nuxt',
+      icon: 'simple-icons:nuxtdotjs',
+      type: TechType.Framework,
+    },
+    {
+      title: 'Laravel',
+      icon: 'simple-icons:laravel',
+      type: TechType.Framework,
+    },
+    {
+      title: 'NodeJS',
+      icon: 'simple-icons:nodedotjs',
+      type: TechType.Framework,
+    },
+    {
+      title: 'Tailwind CSS',
+      icon: 'simple-icons:tailwindcss',
+      type: TechType.Framework,
+    },
+    {
+      title: 'Typescript',
+      icon: 'simple-icons:typescript',
+      type: TechType.Language,
+    },
+    {
+      title: 'PHP',
+      icon: 'simple-icons:php',
+      type: TechType.Language,
+    },
+    {
+      title: 'Javascript',
+      icon: 'simple-icons:javascript',
+      type: TechType.Language,
+    },
+    {
+      title: 'Vitest',
+      icon: 'simple-icons:vitest',
+      type: TechType.Testing,
+    },
+    {
+      title: 'Playwright',
+      icon: 'simple-icons:playwright',
+      type: TechType.Testing,
+    },
+    {
+      title: 'MongoDB',
+      icon: 'simple-icons:mongodb',
+      type: TechType.Db,
+    },
+    {
+      title: 'Postgresql',
+      icon: 'simple-icons:postgresql',
+      type: TechType.Db,
+    },
+    {
+      title: 'Redis',
+      icon: 'simple-icons:redis',
+      type: TechType.Db,
+    },
+    {
+      title: 'RabbitMQ',
+      icon: 'simple-icons:rabbitmq',
+      type: TechType.Db,
+    },
+    {
+      title: 'Docker',
+      icon: 'simple-icons:docker',
+      type: TechType.DevOps,
+    },
+    {
+      title: 'Jenkins',
+      icon: 'simple-icons:jenkins',
+      type: TechType.DevOps,
+    },
+    {
+      title: 'Github Actions',
+      icon: 'simple-icons:githubactions',
+      type: TechType.DevOps,
+    },
+    {
+      title: 'Grafana',
+      icon: 'simple-icons:grafana',
+      type: TechType.DevOps,
+    },
+    {
+      title: 'Jira',
+      icon: 'simple-icons:jira',
+      type: TechType.Tools,
+    },
+    {
+      title: 'VSCode',
+      icon: 'simple-icons:visualstudiocode',
+      type: TechType.Tools,
+    },
+    {
+      title: 'PHPStorm',
+      icon: 'simple-icons:phpstorm',
+      type: TechType.Tools,
+    },
+    {
+      title: 'NeoVIM',
+      icon: 'simple-icons:neovim',
+      type: TechType.Tools,
+    },
+  ];
+
   return {
-    technologies: [
-      {
-        title: 'Vue',
-        icon: 'simple-icons:vuedotjs',
-        type: TechType.Framework,
-      },
-      {
-        title: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        type: TechType.Framework,
-      },
-      {
-        title: 'Laravel',
-        icon: 'simple-icons:laravel',
-        type: TechType.Framework,
-      },
-      {
-        title: 'NodeJS',
-        icon: 'simple-icons:nodedotjs',
-        type: TechType.Framework,
-      },
-      {
-        title: 'Tailwind CSS',
-        icon: 'simple-icons:tailwindcss',
-        type: TechType.Framework,
-      },
-      {
-        title: 'Typescript',
-        icon: 'simple-icons:typescript',
-        type: TechType.Language,
-      },
-      {
-        title: 'PHP',
-        icon: 'simple-icons:php',
-        type: TechType.Language,
-      },
-      {
-        title: 'Javascript',
-        icon: 'simple-icons:javascript',
-        type: TechType.Language,
-      },
-      {
-        title: 'Vitest',
-        icon: 'simple-icons:vitest',
-        type: TechType.Testing,
-      },
-      {
-        title: 'Playwright',
-        icon: 'simple-icons:playwright',
-        type: TechType.Testing,
-      },
-      {
-        title: 'MongoDB',
-        icon: 'simple-icons:mongodb',
-        type: TechType.Db,
-      },
-      {
-        title: 'Postgresql',
-        icon: 'simple-icons:postgresql',
-        type: TechType.Db,
-      },
-      {
-        title: 'Redis',
-        icon: 'simple-icons:redis',
-        type: TechType.Db,
-      },
-      {
-        title: 'RabbitMQ',
-        icon: 'simple-icons:rabbitmq',
-        type: TechType.Db,
-      },
-      {
-        title: 'Docker',
-        icon: 'simple-icons:docker',
-        type: TechType.DevOps,
-      },
-      {
-        title: 'Jenkins',
-        icon: 'simple-icons:jenkins',
-        type: TechType.DevOps,
-      },
-      {
-        title: 'Github Actions',
-        icon: 'simple-icons:githubactions',
-        type: TechType.DevOps,
-      },
-      {
-        title: 'Grafana',
-        icon: 'simple-icons:grafana',
-        type: TechType.DevOps,
-      },
-      {
-        title: 'Jira',
-        icon: 'simple-icons:jira',
-        type: TechType.Tools,
-      },
-      {
-        title: 'VSCode',
-        icon: 'simple-icons:visualstudiocode',
-        type: TechType.Tools,
-      },
-      {
-        title: 'PHPStorm',
-        icon: 'simple-icons:phpstorm',
-        type: TechType.Tools,
-      },
-      {
-        title: 'NeoVIM',
-        icon: 'simple-icons:neovim',
-        type: TechType.Tools,
-      },
+    technologies,
+    types: [
+      TechType.Framework,
+      TechType.Language,
+      TechType.Testing,
+      TechType.Db,
+      TechType.DevOps,
+      TechType.Tools,
     ],
   };
 }
